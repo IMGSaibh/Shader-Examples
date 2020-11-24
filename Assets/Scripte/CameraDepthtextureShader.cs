0ee1978354af708003eb21cb47351723405c6622
IMGSaibh <murmelauge87@gmx.de>
2020-11-24 23:44:36 +0100

    added outline advanced 2

---
+++ Assets/Scripte/CameraDepthtextureShader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDepthtextureShader : MonoBehaviour
+{
+    public Shader depthtextureShader;
+    Material depthtextureMaterial;
+    [Range(0.0f, 1.0f)]
+    public float depthPower = 1.0f; 
+    void Start()
+    {
+        //check if shader is assigned and supported
+        if (!depthtextureShader && !depthtextureShader.isSupported) 
+        {
+            Debug.Log("Shader not assigned or not supported");
+            enabled = false;
+        }
+
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+        Debug.Log("Camera depthTextureMode: " + Camera.main.depthTextureMode);
+        depthtextureMaterial = new Material(depthtextureShader);
+
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        if (depthtextureShader != null)
+        {
+            depthtextureMaterial.SetFloat("_DepthPower", depthPower);
+            Graphics.Blit(source, destination, depthtextureMaterial);
+
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+        depthPower = Mathf.Clamp(depthPower, 0, 5);
+    }
+
+    private void OnDisable()
+    {
+        if (depthtextureMaterial)
+        {
+            DestroyImmediate(depthtextureMaterial);
+        }
+    }
+}
---
+++ Assets/Scripte/CameraOutline.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraOutline : MonoBehaviour
+{
+
+    //For Antialaising in Texture draw before post processing 
+    const CameraEvent cameraEvent = CameraEvent.AfterForwardAlpha;
+    CommandBuffer commandBuffer;
+
+    public Camera mainCamera;
+    public Material maskingShaderMaterial;
+    public Material outlineShaderMaterial;
+
+    public List<Renderer> objectRenderer = new List<Renderer>();
+
+    [Range(1, 30)]
+    public float thickness = 1;
+    public Color color;
+
+    private int outlineTextureID;
+    private int outlineSizeID;
+    private int outlineColorID;
+
+    public bool showMaskingTexture;
+
+
+    private void OnEnable()
+    {
+
+        outlineTextureID = Shader.PropertyToID("_HighlightTexture");
+        outlineSizeID = Shader.PropertyToID("_Size");
+        outlineColorID = Shader.PropertyToID("_ColorMain");
+        BuildComandBuffer();
+
+    }
+
+    private void OnDisable()
+    {
+        if (mainCamera != null && commandBuffer != null)
+        {
+            mainCamera.RemoveCommandBuffer(cameraEvent, commandBuffer);
+            commandBuffer = null;
+        }
+    }
+
+    private void BuildComandBuffer()
+    {
+        if (mainCamera == null)
+        {
+            return;
+        }
+
+        if (commandBuffer == null)
+        {
+            commandBuffer = new CommandBuffer();
+            commandBuffer.name = "Postprocessing";
+            mainCamera.AddCommandBuffer(cameraEvent, commandBuffer);
+        }
+        commandBuffer.Clear();
+
+        // Can't do shit without these
+        if (maskingShaderMaterial == null || outlineShaderMaterial == null)
+        {
+            Debug.LogError("No Material is set");
+            return;
+
+        }
+
+        commandBuffer.GetTemporaryRT(outlineTextureID, mainCamera.pixelWidth, mainCamera.pixelHeight, 24, FilterMode.Point, RenderTextureFormat.R8);
+        commandBuffer.SetRenderTarget(outlineTextureID);
+        commandBuffer.ClearRenderTarget(true, true, Color.black);
+
+        foreach (Renderer render in objectRenderer)
+        {
+            if (!render)
+                continue;
+
+            commandBuffer.DrawRenderer(render, maskingShaderMaterial);
+        }
+
+        //Draw image effect
+        commandBuffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+        commandBuffer.Blit(outlineTextureID, BuiltinRenderTextureType.CameraTarget, showMaskingTexture ? null : outlineShaderMaterial);
+        commandBuffer.ReleaseTemporaryRT(outlineTextureID);
+
+        outlineShaderMaterial.SetFloat(outlineSizeID, thickness);
+        outlineShaderMaterial.SetColor(outlineColorID, color);
+
+    }
+    public void ReleaseAll()
+    {
+        if (mainCamera != null && commandBuffer != null)
+        {
+            mainCamera.RemoveCommandBuffer(cameraEvent, commandBuffer);
+            commandBuffer = null;
+        }
+    }
+    private void OnValidate()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = GetComponent<Camera>();
+        }
+
+        if (mainCamera != null && mainCamera.depthTextureMode == DepthTextureMode.None)
+        {
+            mainCamera.depthTextureMode = DepthTextureMode.Depth;
+        }
+        //ReleaseAll();
+
+        BuildComandBuffer();
+    }
+
+}
